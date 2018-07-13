576c425d196ee2d790c0956f2bacdc788f1ce6c1
Terry Teng <terry.teng@oocl.com>
2018-07-13 15:20:56 +0800

    re

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,24 +6,20 @@ import java.util.*;
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
-        } else {
+        //split the input string with 1 to n pieces of spaces
+        List<Input> inputList = getInputList(inputStr);
 
-            //split the input string with 1 to n pieces of spaces
-            List<Input> inputList = getInputList(inputStr);
+        //get the map for the next step of sizing the same word
+        Map<String, List<Input>> map = getListMap(inputList);
 
-            //get the map for the next step of sizing the same word
-            Map<String, List<Input>> map = getListMap(inputList);
+        List<Word> resultList = getModel(map);
 
-            List<Word> resultList = getWords(map);
+        return getView(resultList);
 
-            return getString(resultList);
 
-        }
     }
 
-    private String getString(List<Word> resultList) {
+    private String getView(List<Word> resultList) {
         StringJoiner joiner = new StringJoiner("\n");
         for (Word w : resultList) {
             String s = w.getValue() + " " + w.getWordCount();
@@ -32,15 +28,15 @@ public class WordFrequencyGame {
         return joiner.toString();
     }
 
-    private List<Word> getWords(Map<String, List<Input>> map) {
-        List<Word> resultList = new ArrayList<>();
+    private List<Word> getModel(Map<String, List<Input>> map) {
+        List<Word> listModel = new ArrayList<>();
         for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
             Word input = new Word(entry.getKey(), entry.getValue().size());
-            resultList.add(input);
+            listModel.add(input);
         }
 
-        resultList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-        return resultList;
+        listModel.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        return listModel;
     }
 
     private List<Input> getInputList(String inputStr) {
